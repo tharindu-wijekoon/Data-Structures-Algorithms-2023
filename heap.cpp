7f3110c60f2068ed7de60756144bd5d86f2f2204
tharindu-wijekoon <130040948+tharindu-wijekoon@users.noreply.github.com>
2023-05-04 17:27:45 +0530

    Add files via upload

---
+++ heap.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+  
+// function to heapify the tree
+void heapify(int arr[], int n, int root)
+{
+   // build heapify
+   int bigger = root; // take root as the large number
+   int left = 2 * root + 1; // placing the left child
+   int right = 2 * root + 2; //placing the right child
+
+   if (left < n && arr[left] > arr[bigger]) //find the correct value for left child
+      bigger = left;
+
+   if (right < n && arr[right] > arr[bigger]) // find the correct value for right child
+      bigger = right;
+
+   if (bigger != root) { // if root is not the large number
+      int temp = arr[root];
+      arr[root] = arr[bigger];
+      arr[bigger] = temp;
+      heapify(arr, n, bigger);
+   }  
+}
+  
+// implementing heap sort
+void heapSort(int arr[], int n)
+{
+   // build heap
+   for (int i = n / 2 - 1; i >= 0; i--){
+         heapify(arr, n, i);
+      }
+   // extracting elements from heap one by one
+   for (int i = n - 1; i >= 0; i--) {
+         int temp = arr[0];
+         arr[0] = arr[i];
+         arr[i] = temp;
+         heapify(arr, i, 0);
+    }
+
+}
+  
+/* print contents of array */
+void displayArray(int arr[], int n)
+{
+   for (int i=0; i<n; ++i)
+   cout << arr[i] << " ";
+   cout << "\n";
+}
+  
+// main program
+int main()
+{
+   int heap_arr[] = {4,17,3,12,9,6};
+   int n = sizeof(heap_arr)/sizeof(heap_arr[0]);
+   cout<<"Input array"<<endl;
+   displayArray(heap_arr,n);
+  
+   heapSort(heap_arr, n);
+  
+   cout << "Sorted array"<<endl;
+   displayArray(heap_arr, n);
+}
